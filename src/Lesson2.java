7a63ca381678aafae51a0aa226ebee4af3947641
Євген Ткачук <tkacukevgen96@gmail.com>
2025-08-24 16:16:56 +0300

    Ex.2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,15 +1,7 @@
 public class Lesson2 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int temp;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println(" ");
-        temp = a;
-        a = b;
-        b = temp;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        double odessaLatitude = 46.488084;
+        double odessaLongitude = 30.741084;
+        System.out.println("\nOdessa:\nLatitude  " + odessaLatitude + ";\nLongitude " + odessaLongitude + ";\n");
     }
 }
