2777eae1ff4ad85080e6e6c92541867f12cf2fcf
Євген Ткачук <tkacukevgen96@gmail.com>
2025-08-24 15:47:04 +0300

    Ex.1

---
+++ src/Lesson2.java
@@ -0,0 +1,15 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println(" ");
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
