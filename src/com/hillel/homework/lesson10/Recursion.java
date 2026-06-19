d10c4e5c1e11c34591ad5a06a11ea869f5244db0
Євген Ткачук <tkacukevgen96@gmail.com>
2026-06-19 23:35:39 +0300

    Add Recursion.java

--- src/com/hillel/homework/lesson10/Recursion.java
+++ src/com/hillel/homework/lesson10/Recursion.java
@@ -1,7 +1,21 @@
 package com.hillel.homework.lesson10;
 
-public class Recurtion {
+import java.util.Scanner;
+
+public class Recursion {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введіть число для обчислення факторіалу: ");
+        int num = sc.nextInt();
+        System.out.println("Результат: ");
+        System.out.println(recursion(num));
+    }
+
+    public static int recursion(int num) {
+        if (num <= 1) {
+            return 1;
+        }
+        num *= recursion(num - 1);
+        return num;
     }
 }
