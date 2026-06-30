91b15679224b7f59a4b03c2dafd42d7313535057
Євген Ткачук <tkacukevgen96@gmail.com>
2026-06-30 18:27:03 +0300

    Refactored Cat.java

--- src/com/hillel/classwork/lesson13/Cat.java
+++ src/com/hillel/classwork/lesson13/Cat.java
@@ -1,9 +1,5 @@
 package com.hillel.classwork.lesson13;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Cat {
     private String name;
     private int age;
@@ -23,37 +19,26 @@ public class Cat {
     public Cat fight(Cat catOpponent) {
         if (this.strength > catOpponent.strength) {
             return this;
-        } else if (this.strength == catOpponent.strength) {
-            if (this.weight > catOpponent.weight) {
+        }
+        if (this.strength == catOpponent.strength) {
+            if (this.weight > catOpponent.weight)
                 return this;
-            } else if (this.weight == catOpponent.weight) {
-                if (this.age < catOpponent.age) {
-                    return this;
-                } else if (this.age == catOpponent.age) {
-                    if (this.streetCat && !catOpponent.streetCat) {
-                        return this;
-                    }
-                    if (this.streetCat == catOpponent.streetCat) {
-                        return null;
-                    }
-                }
-            }
+        }
+        if (this.weight == catOpponent.weight) {
+            if (this.age < catOpponent.age)
+                return this;
+        }
+        if (this.age == catOpponent.age) {
+            if (this.streetCat && !catOpponent.streetCat)
+                return this;
+
+        }
+        if (this.streetCat == catOpponent.streetCat) {
+            return null;
         }
         return catOpponent;
     }
 
-/*    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Cat) {
-            Cat cat = (Cat) obj;
-            return this.strength == cat.strength;
-        }
-        return false;
-    }
-    public int hashCode(){
-        return Objects.hash(strength);
-    }
-*/
 
     public String getName() {
         return name;
@@ -88,18 +73,6 @@ public class Cat {
         return (int) coef;
     }
 
-    public static void printList(Cat cat1, Cat cat2, Cat cat3, Cat cat4, Cat cat5) {
-        List<Cat> catList = new ArrayList<>();
-        catList.add(cat1);
-        catList.add(cat2);
-        catList.add(cat3);
-        catList.add(cat4);
-        catList.add(cat5);
-
-        for (Cat cat : catList) {
-            System.out.println(cat);
-        }
-    }
 
     @Override
     public String toString() {
